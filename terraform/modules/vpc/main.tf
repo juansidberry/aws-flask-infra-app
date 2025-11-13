f16332e14eedc3aa1abb862b0cd041f37cb35231
Juan Sidberry <jsidberry@gmail.com>
2025-11-13 11:46:36 -0500

    clean up formatting for cleaner readbility

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -41,22 +41,33 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = each.value
   availability_zone = element(var.azs, index(var.private_subnet_cidrs, each.value))
-  tags = { Name = "${var.name}-private-${substr(replace(each.value, ".", "-", ),8,4)}" }
+  tags = { 
+    Name = "${var.name}-private-${substr(replace(each.value, ".", "-", ),8,4)}" 
+  }
 }
 
-resource "aws_eip" "nat" { domain = "vpc" }
+resource "aws_eip" "nat" { 
+  domain = "vpc"
+}
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = values(aws_subnet.public)[0].id
-  tags = { Name = "${var.name}-nat" }
+  tags = { 
+    Name = "${var.name}-nat" 
+  }
   depends_on = [aws_internet_gateway.igw]
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
-  route { cidr_block = "0.0.0.0/0"; gateway_id = aws_internet_gateway.igw.id }
-  tags = { Name = "${var.name}-public-rt" }
+  route { 
+    cidr_block = "0.0.0.0/0" 
+    gateway_id = aws_internet_gateway.igw.id 
+  }
+  tags = { 
+    Name = "${var.name}-public-rt" 
+  }
 }
 
 resource "aws_route_table_association" "public" {
@@ -67,8 +78,13 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
-  route { cidr_block = "0.0.0.0/0"; nat_gateway_id = aws_nat_gateway.nat.id }
-  tags = { Name = "${var.name}-private-rt" }
+  route { 
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id 
+  }
+  tags = { 
+    Name = "${var.name}-private-rt" 
+  }
 }
 
 resource "aws_route_table_association" "private" {
@@ -81,18 +97,50 @@ resource "aws_route_table_association" "private" {
 resource "aws_network_acl" "public" {
   vpc_id = aws_vpc.this.id
   subnet_ids = [for s in aws_subnet.public : s.id]
-  ingress { rule_no=100 protocol="tcp" action="allow" cidr_block="0.0.0.0/0" from_port=80  to_port=80 }
-  ingress { rule_no=110 protocol="tcp" action="allow" cidr_block="0.0.0.0/0" from_port=443 to_port=443 }
-  egress  { rule_no=100 protocol="-1"   action="allow" cidr_block="0.0.0.0/0" from_port=0   to_port=0 }
-  tags = { Name = "${var.name}-public-nacl" }
+  ingress { 
+    rule_no=100 
+    protocol="tcp" 
+    action="allow" 
+    cidr_block="0.0.0.0/0" 
+    from_port=80  
+    to_port=80 
+  }
+  ingress { 
+    rule_no=110 
+    protocol="tcp" 
+    action="allow" 
+    cidr_block="0.0.0.0/0" 
+    from_port=443 
+    to_port=443 
+  }
+  egress { 
+    rule_no=100 
+    protocol="-1"   
+    action="allow" 
+    cidr_block="0.0.0.0/0" 
+    from_port=0   
+    to_port=0 
+  }
+  tags = { 
+    Name = "${var.name}-public-nacl" 
+  }
 }
 
 resource "aws_network_acl" "private" {
   vpc_id = aws_vpc.this.id
   subnet_ids = [for s in aws_subnet.private : s.id]
   # allow ephemeral outbound and established return; block inbound by default
-  egress  { rule_no=100 protocol="-1" action="allow" cidr_block="0.0.0.0/0" from_port=0 to_port=0 }
-  tags = { Name = "${var.name}-private-nacl" }
+  egress  { 
+    rule_no=100 
+    protocol="-1" 
+    action="allow" 
+    cidr_block="0.0.0.0/0" 
+    from_port=0 
+    to_port=0 
+  }
+  tags = { 
+    Name = "${var.name}-private-nacl" 
+  }
 }
 
 output "vpc_id"                { value = aws_vpc.this.id }
