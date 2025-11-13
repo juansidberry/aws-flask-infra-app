556e91cbef1233361f5c39c12f8b76071a15945c
Juan Sidberry <jsidberry@gmail.com>
2025-11-13 11:41:06 -0500

    formatting and notes/doc

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -16,7 +16,12 @@ resource "aws_security_group" "db_sg" {
     security_groups = [var.app_sg_id]
   }
 
-  egress { from_port=0 to_port=0 protocol="-1" cidr_blocks=["0.0.0.0/0"] }
+  egress { 
+    from_port=0 
+    to_port=0 
+    protocol="-1" 
+    cidr_blocks=["0.0.0.0/0"] 
+  }
 }
 
 resource "aws_db_instance" "this" {
