2f5d7141ee5651fba918651e93b576e148a825cd
Juan Sidberry <jsidberry@gmail.com>
2025-11-13 11:51:02 -0500

    clean up formatting for cleaner readbility

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,9 +1,24 @@
 resource "aws_security_group" "alb_sg" {
   name   = "${var.name}-alb-sg"
   vpc_id = var.vpc_id
-  ingress { from_port=80  to_port=80  protocol="tcp" cidr_blocks=["0.0.0.0/0"] }
-  ingress { from_port=443 to_port=443 protocol="tcp" cidr_blocks=["0.0.0.0/0"] }
-  egress  { from_port=0   to_port=0   protocol="-1"  cidr_blocks=["0.0.0.0/0"] }
+  ingress { 
+    from_port=80  
+    to_port=80  
+    protocol="tcp" 
+    cidr_blocks=["0.0.0.0/0"] 
+  }
+  ingress { 
+    from_port=443 
+    to_port=443 
+    protocol="tcp" 
+    cidr_blocks=["0.0.0.0/0"] 
+  }
+  egress { 
+    from_port=0   
+    to_port=0   
+    protocol="-1"  
+    cidr_blocks=["0.0.0.0/0"] 
+  }
 }
 
 resource "aws_lb" "alb" {
@@ -18,14 +33,20 @@ resource "aws_lb_target_group" "tg" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-  health_check { path = "/health" matcher = "200-399" }
+  health_check { 
+    path = "/health" 
+    matcher = "200-399" 
+  }
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.alb.arn
   port = 80
   protocol = "HTTP"
-  default_action { type = "forward" target_group_arn = aws_lb_target_group.tg.arn }
+  default_action { 
+    type = "forward" 
+    target_group_arn = aws_lb_target_group.tg.arn 
+  }
 }
 
 output "alb_dns_name" { value = aws_lb.alb.dns_name }
